c4b597390095737a38a4ca2fcded643647c867ce
kahbyte <kaue.sbsousa@gmail.com>
2020-11-12 15:56:47 -0300

    First version of the export function ready.

--- PixelArt App/PixelArt App/ViewController.swift
+++ PixelArt App/PixelArt App/ViewController.swift
@@ -14,37 +14,31 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     /*copy the grid's view, remove it's borders, rescale it and then pops up a share sheet to export it*/
     @IBAction func export(_ sender: Any) {
-        testView.removerBordas()
-        
         UIView.animate(withDuration: 0.3) {
             self.scrollView.zoomScale = 1.0
         }
         
-        //let scaledGridView = scaleViewsToHD(view: testView.contentView)
+        let scaledGridView = scaleViewsToHD(view: testView.contentView)
         
-        let renderer = UIGraphicsImageRenderer(size: testView.bounds.size)
+        let renderer = UIGraphicsImageRenderer(size: scaledGridView.bounds.size)
         
         
         let image = renderer.image { ctx in
-            testView.drawHierarchy(in: testView.bounds, afterScreenUpdates: true)
+            scaledGridView.drawHierarchy(in: scaledGridView.bounds, afterScreenUpdates: true)
         }
         
         let share = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-        
-        share.completionWithItemsHandler = { activity, success, items, error in
-            self.testView.adicionarBordas()
-        }
-        
+
         present(share, animated: true, completion: nil)
         
-        //scaledGridView.removeFromSuperview()
-        //UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        scaledGridView.removeFromSuperview()
     }
     
     /*Scales the view and everything in it*/
     func scaleViewsToHD(view: UIView) -> UIView {
+        let newCells: [String: UIView] = testView.cells
         
-        let hdView = view.copyView()
+        let hdView = UIView()
         
         hdView.frame.size = CGSize(width: 1080, height: 1080)
         
@@ -56,11 +50,16 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         
         /*scalling everything
          I've been through hell*/
-        
-        for subview in hdView.subviews {
-            subview.frame.size = CGSize(width: subview.frame.width * scaleMultiplier, height: subview.frame.height * scaleMultiplier)
-            subview.layer.borderWidth = 0.8
+        for j in 1 ... 31 {
+            for i in 1 ... 31 {
+                let key = "\(i)|\(j)"
+                let cell = newCells[key]?.copyView()
+                cell?.layer.borderWidth = 0.0
+                cell!.frame = CGRect(x: width * CGFloat(i-1), y: width * CGFloat(j-1), width: width, height: width)
+                hdView.addSubview(cell!)
+            }
         }
+        
         return hdView
     }
     
